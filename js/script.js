ade41c483af4aa0ecda6cc61f091d7067c4f14cd
tung2901 <tranhuutungkeu@gmail.com>
2024-09-13 18:37:40 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ---------------------menu-sliderbar
+const itemsliderbar = document.querySelectorAll(".cartegory-left-li")
+itemsliderbar.forEach(function(menu,index){
+    menu.addEventListener("click", function(){
+        menu.classList.toggle("block")
+    })
+}) 
