232f6cb273eae7f9e7c04db5b76b3697a93321be
tung2901 <tranhuutungkeu@gmail.com>
2024-09-18 12:08:03 +0700

    add

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-// ---------------------menu-sliderbar
+//------------------------Menu-item------------------------
+const toP = document.querySelector(".top")
+window.addEventListener("scroll", function(){
+    const X = this.pageYOffset;
+    if(X > 1){toP.classList.add("active")}
+    else {toP.classList.remove("active")}
+})
+
+//------------------------Menu-SLIDEBAR-CATEGORY------------------------
 const itemsliderbar = document.querySelectorAll(".cartegory-left-li")
-itemsliderbar.forEach(function(menu,index){
+itemsliderbar.forEach(function(menu, index){
     menu.addEventListener("click", function(){
         menu.classList.toggle("block")
     })
-}) 
+})
+// ------------------------product------------------
+const baoquan = document.querySelector(".baoquan")
+const chitiet = document.querySelector(".chitiet")
+if(baoquan) {
+    baoquan.addEventListener("click", function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display="none"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display="block"
+    })
+}
